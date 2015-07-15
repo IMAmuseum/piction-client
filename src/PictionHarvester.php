c8499c6a11a0684dc1479d738821aa637c6ae821
Todd Kersey <tkersey@imamuseum.org>
2015-07-15 12:16:59 -0400

    Update harvester to include id specific lookups and update images array layout.

--- src/PictionHarvester.php
+++ src/PictionHarvester.php
@@ -15,94 +15,20 @@ class PictionHarvester
 
         $this->image_url = getenv('PICTION_IMAGE_URL');
 
-        $this->age = 10;
-        $this->maxrows = 1500;
+        $this->age = 30;
+        $this->maxrows = 1000;
         $this->metatags = 'IMA.PUBLICLY AVAILABLE,IMA.IRN';
+        $this->start = 0;
 
         $this->id_field = 'IMA.IRN';
 
         $this->collection_id = 'AID:7912565';
 
-    }
+        $this->ID = "IRN";
 
-    public function getAllObjects()
-    {
-        $piction_method = 'image_query';
+        $this->IMAGE_TO_PULL = "Original Asset";
 
-        if(isset($this->collection_id) && $this->collection_id != ""){
-            $params = array(
-                'SEARCH' => $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
-                'FORCE_REFRESH' => True,
-                'METADATA' => True,
-                'MAXROWS' => $this->maxrows
-            );
-        } else {
-            $params = array(
-                'SEARCH' => 'IMAGE_TYPE:PHOTO',
-                'FORCE_REFRESH' => True,
-                'METADATA' => True,
-                'MAXROWS' => $this->maxrows
-            );
-        }
-
-        $data = $this->piction->call($piction_method, $params);
-
-        $data = $this->transformData($data);
-
-        return $data;
-    }
-
-    public function getUpdatedObjects()
-    {
-        $piction_method = 'image_query';
-        if(isset($this->collection_id) && $this->collection_id != ""){
-            $params = array(
-                'SEARCH' => 'AGE:' . $this->age . ' AND ' . $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
-                'FORCE_REFRESH' => True,
-                'METADATA' => True,
-                //'METATAGS' => $this->metatags,
-                'MAXROWS' => $this->maxrows
-            );
-        } else {
-            $params = array(
-                'SEARCH' => 'AGE:' . $this->age . ' AND IMAGE_TYPE:PHOTO',
-                'FORCE_REFRESH' => True,
-                'METADATA' => True,
-                'MAXROWS' => $this->maxrows
-            );
-        }
-
-        $data = $this->piction->call($piction_method, $params);
-
-        $data = $this->transformData($data);
-
-        return $data;
-    }
-
-    public function getSpecificObject($id)
-    {
-        $piction_method = 'image_query';
-        $params = array(
-            'SEARCH' => 'META:"' . $this->id_field . ',' . $id . '"',
-            'FORCE_REFRESH' => True,
-            'METADATA' => True,
-            'MAXROWS' => $this->maxrows
-        );
-
-        $data = $this->piction->call($piction_method, $params);
-
-        $data = $this->transformData($data);
-
-        return $data;
-    }
-
-    public function transformData($data)
-    {
-        $ID_FIELD = "IRN";
-
-        $IMAGE_TO_PULL = "Original Asset";
-
-        $FIELD_MAP = array(
+        $this->FIELD_MAP = array(
             "IRN" => "id",
             "TITACCESSIONNO" => "accession_num",
             "TITACCESSIONDATE" => "accession_date",
@@ -132,19 +58,161 @@ class PictionHarvester
             "YEAR" => "year"
         );
 
+    }
+
+    public function getAllObjects()
+    {
+        $piction_method = 'image_query';
+
+        if(isset($this->collection_id) && $this->collection_id != ""){
+            $params = array(
+                'SEARCH' => $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METADATA' => True,
+                'MAXROWS' => $this->maxrows,
+                'ORDERBY' => $this->id_field
+            );
+        } else {
+            $params = array(
+                'SEARCH' => 'IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METADATA' => True,
+                'MAXROWS' => $this->maxrows,
+                'ORDERBY' => $this->id_field
+            );
+        }
+
+        // Make piction call
+        $data = $this->piction->call($piction_method, $params);
+
+        // Transform data into something more manageable
+        $data = $this->transformData($data);
+
+        return $data;
+    }
+
+    public function getUpdatedObjects()
+    {
+        $piction_method = 'image_query';
+        if(isset($this->collection_id) && $this->collection_id != ""){
+            $params = array(
+                'SEARCH' => 'AGE:' . $this->age . ' AND ' . $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METADATA' => True,
+                'MAXROWS' => $this->maxrows,
+                'ORDERBY' => $this->id_field
+            );
+        } else {
+            $params = array(
+                'SEARCH' => 'AGE:' . $this->age . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METADATA' => True,
+                'MAXROWS' => $this->maxrows,
+                'ORDERBY' => $this->id_field
+            );
+        }
+
+        // Make piction call
+        $data = $this->piction->call($piction_method, $params);
+
+        // Transform data into something more manageable
+        $data = $this->transformData($data);
+
+        return $data;
+    }
+
+    public function getSpecificObject($id)
+    {
+        $piction_method = 'image_query';
+        $params = array(
+            'SEARCH' => 'META:"' . $this->id_field . ',' . $id . '"',
+            'FORCE_REFRESH' => True,
+            'METADATA' => True,
+            'MAXROWS' => $this->maxrows
+        );
+
+        // Make piction call
+        $data = $this->piction->call($piction_method, $params);
+
+        // Transform data into something more manageable
+        $data = $this->transformData($data);
+
+        return $data;
+    }
+
+    // Get just the ids of all objects
+    public function getAllObjectIDs() {
+        $piction_method = 'image_query';
+        if(isset($this->collection_id) && $this->collection_id != ""){
+            $params = array(
+                'SEARCH' => $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METATAGS' => $this->id_field,
+                'ORDERBY' => $this->id_field,
+                'MAXROWS' => 1000
+            );
+        } else {
+            $params = array(
+                'SEARCH' => 'IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METATAGS' => $this->id_field,
+                'ORDERBY' => $this->id_field,
+                'MAXROWS' => 1000
+            );
+        }
+
+        // Make piction call
+        $data = $this->piction->call($piction_method, $params);
+
+        // Transform data into something more manageable
+        $data = $this->transformIdData($data);
+
+        return $data;
+    }
+
+    // Get just the ids of objects that have been updated
+    public function getUpdatedObjectIDs() {
+        $piction_method = 'image_query';
+        if(isset($this->collection_id) && $this->collection_id != ""){
+            $params = array(
+                'SEARCH' => 'AGE:' . $this->age . ' AND ' . $this->collection_id . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METATAGS' => $this->id_field,
+                'ORDERBY' => $this->id_field,
+                'MAXROWS' => 10000
+            );
+        } else {
+            $params = array(
+                'SEARCH' => 'AGE:' . $this->age . ' AND IMAGE_TYPE:PHOTO',
+                'FORCE_REFRESH' => True,
+                'METATAGS' => $this->id_field,
+                'ORDERBY' => $this->id_field,
+                'MAXROWS' => 10000
+            );
+        }
+
+        // Make piction call
+        $data = $this->piction->call($piction_method, $params);
+
+        // Transform data into something more manageable
+        $data = $this->transformIdData($data);
+
+        return $data;
+    }
+
+    public function transformData($data)
+    {
         // Get data from Piction
         $data = json_decode($data, true);
 
         // Create new array to store the transformed data
         $newData = array();
-        // $newData['total'] = $data['s']['t'];
+        $newData['total'] = $data['s']['t'];
 
         // Some book keeping variables and arrays
         $found_file = array();
         $found_ids = array();
         $current_id = 0;
-        $priority_primary = 0;
-        $priority_secondary = 0;
 
         // Loop through results items
         foreach($data['r'] as $result) {
@@ -153,14 +221,14 @@ class PictionHarvester
                 foreach($result['m'] as $metadata) {
 
                     // check if the metadata element is in our field mapping
-                    if(array_key_exists($ID_FIELD, $FIELD_MAP)) {
+                    if(array_key_exists($this->ID, $this->FIELD_MAP)) {
 
                         // Since piction stores the name of the field as a value and the value as another value
                         // we have to loop through the metadata to store the id
                         foreach($metadata as $k => $v) {
 
                             // If the current value matches the id field name
-                            if ($v == $ID_FIELD){
+                            if ($v == $this->ID){
 
                                 // Store the value in a variable and only once in the found_ids array
                                 $current_id = $metadata['v'];
@@ -181,18 +249,18 @@ class PictionHarvester
                     foreach($result['m'] as $metadata) {
 
                         // check if the metadata element is in our field mapping
-                        if(array_key_exists($metadata['c'], $FIELD_MAP)) {
+                        if(array_key_exists($metadata['c'], $this->FIELD_MAP)) {
 
                             // Store metadata item if doesn't currently exist or if the current value is blank
-                            if (!isset($newData['results'][$current_id][$FIELD_MAP[$metadata['c']]]) || $newData['results'][$current_id][$FIELD_MAP[$metadata['c']]] == "") {
-                                $newData['results'][$current_id][$FIELD_MAP[$metadata['c']]] = htmlspecialchars($metadata['v']);
+                            if (!isset($newData['results'][$current_id][$this->FIELD_MAP[$metadata['c']]]) || $newData['results'][$current_id][$this->FIELD_MAP[$metadata['c']]] == "") {
+                                $newData['results'][$current_id][$this->FIELD_MAP[$metadata['c']]] = htmlspecialchars($metadata['v']);
                             }
                         }
                     }
 
                     // Match for images with v##.jpg at the end of the url
                     // These are the primary images to use on the site.
-                    $matches = preg_match('/(ps.+v\d+)\.(jpg|jpeg)/i', $result['n']);
+                    $matches = preg_match('/\.(jpg|jpeg)/i', $result['n']);
                     if($matches) {
 
                         // If there was a match, add the match to the found files array
@@ -202,45 +270,62 @@ class PictionHarvester
                         foreach($result['o'] as $image) {
 
                             // Check if the current image is the one we want
-                            if ($image['pn'] == $IMAGE_TO_PULL){
+                            if ($image['pn'] == $this->IMAGE_TO_PULL){
 
                                 // Create array of image data
                                 $img_json = array(
-                                    'source_url' => $this->image_url . $image['u'],
-                                    'priority' => $priority_primary
+                                    'source_url' => $this->image_url . $image['u']
                                 );
 
                                 // Store image data in final json
-                                $newData['results'][$current_id]['images']['primary'][] = $img_json;
-
-                                $priority_primary = count($newData['results'][$current_id]['images']['primary']);
+                                $newData['results'][$current_id]['images'][] = $img_json;
                             }
                         }
-                    } else {
-                        // Match for images with d##.jpg at the end of the url
-                        // These are the secondary images to use on the site.
-                        $matches = preg_match('/(ps.+d\d+)\.(jpg|jpeg)/i', $result['n']);
-                        if($matches) {
+                    }
+                }
+            }
+        }
 
-                            // If there was a match, add the match to the found files array
-                            array_push($found_file, $result['n']);
+        return json_encode($newData);
+    }
 
-                            // Loop through the images in current result
-                            foreach($result['o'] as $image) {
+    // Transform data to only show ID in results
+    public function transformIdData($data)
+    {
+        // Get data from Piction
+        $data = json_decode($data, true);
 
-                                // Check if the current image is the one we want
-                                if ($image['pn'] == $IMAGE_TO_PULL){
+        // Create new array to store the transformed data
+        $newData = array();
+        $newData['total'] = $data['s']['t'];
 
-                                    // Create array of image data
-                                    $img_json = array(
-                                        'source_url' => $this->image_url . $image['u'],
-                                        'priority' => $priority_secondary
-                                    );
+        // Some book keeping variables and arrays
+        $found_ids = array();
+        $current_id = 0;
 
-                                    // Store image data in final json
-                                    $newData['results'][$current_id]['images']['secondary'][] = $img_json;
+        // Loop through results items
+        foreach($data['r'] as $result) {
 
-                                    $priority_secondary = count($newData['results'][$current_id]['images']['secondary']);
+            if($result['t'] == "PHOTO" && (count($result['o']) > 0)) {
+                foreach($result['m'] as $metadata) {
+
+                    // check if the metadata element is in our field mapping
+                    if(array_key_exists($this->ID, $this->FIELD_MAP)) {
+
+                        // Since piction stores the name of the field as a value and the value as another value
+                        // we have to loop through the metadata to store the id
+                        foreach($metadata as $k => $v) {
+
+                            // If the current value matches the id field name
+                            if ($v == $this->ID){
+
+                                // Store the value in a variable and only once in the found_ids array
+                                $current_id = $metadata['v'];
+                                if($current_id != ""){
+                                    if(!in_array($current_id, $found_ids)){
+                                        $found_ids[] = $current_id;
+                                        $newData['results'][] = $metadata['v'];
+                                    }
                                 }
                             }
                         }
